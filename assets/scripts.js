aa0602c047f60f3fd395e7dd6a94e7be0887c46c
maxhannan <maxhannan@substanceseven.com>
2021-02-10 03:20:02 -0600

    Built in AI and refined design

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,37 +1,67 @@
 const boxes = document.querySelectorAll('.box');
 
-const displayControl =(()=>{
+const viewController = (()=>{
+    // DOM CACHE
     const display = document.querySelector('.display');
+    const scoreBoard = document.querySelector('.scoreboard')
     const game = document.querySelector('.gameBoard');
     const menu = document.querySelector('.menu');
-    const render = function(gamestate){
+    let xWins = 0;
+    let yWins = 0;
+    let draws = 0;
+
+    const renderBoard = function(){
+        let gamestate = GameBoard.getCurrentGameState()
+        
         boxes.forEach((box,ix = 0) =>{
-            // console.log(box.innerHTML);
-            box.innerHTML = gamestate[ix];
+            if(gamestate[ix]==='X'||gamestate[ix]==='O'){
+                box.innerHTML = gamestate[ix];
+            }else{
+                box.innerHTML = ''; 
+            }
         })
     };
-    const formHandler = function (){
+
+    const revealBoard = function(){
         menu.style.visibility = 'hidden';
         menu.style.opacity = '0';
         game.style.visibility = 'visible';
         game.style.opacity = '1';
-    }
-    const updateDisplay = function(marker,end = false){
-        if(end){
-            display.innerText = marker;
+        renderBoard();
+    };
+    
+    const controlGameDisplay = function(msg){
+        display.innerText = msg;
+    };
+    const updateScores = function(player,draw = false){
+
+        if(draw){
+            draws++
+            return;
+        }else if(player.getMarker() === 'X'){
+            xWins ++
+            return;
+        }else{
+            yWins++
             return;
         }
-        display.innerText = `${marker}'s turn`
+
     }
-    return {
-        render,
-        updateDisplay,
-        formHandler
+    const controlScoreBoard = function(p1,p2){
+        scoreBoard.innerText = `${p1.getName()}: ${xWins}   ${p2.getName()}: ${yWins}   Draws: ${draws} `
+    };
+
+    return{
+        renderBoard,
+        revealBoard,
+        controlGameDisplay ,
+        updateScores,
+        controlScoreBoard   
     }
 })();
 
 const GameBoard = (()=>{
-    let gamestate = ['','','','','','','','',''];
+    let gameState = [0,1 ,2,3,4,5,6,7,8];
     const winningConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -42,237 +72,271 @@ const GameBoard = (()=>{
         [0, 4, 8],
         [2, 4, 6]
     ];
-    const init = function(){
-        renderGameState();
+    const getCurrentGameState = () => [...gameState];
+
+    const getPossibleMoves = function(state = gameState){
+        return  state.filter(s => s != "O" && s != "X");
     }
-    const getGameState = () => [...gamestate];
-    const renderGameState = function(){
-        displayControl.render(gamestate);
+
+    const updateGameState = function(pos,marker){
+        gameState[pos] = marker;  
     }
-    const validateMove = function(ix){
-        let win = checkWin()
-        // console.log(win)
-        if(gamestate[ix]===''&& !win){
-            return true;
-        }return false;
-    }
-    const updateGameState = function(ix,marker){
-            gamestate[ix]= marker;
-            renderGameState();
-    };
-    const checkWin = function(state = gamestate){
+
+    const checkWin = function(player,state = gameState){
         let win = false;
-        winningConditions.forEach(condition =>{
-            let ix1 = condition[0];
-            let ix2 = condition[1];
-            let ix3 = condition[2];
-            let gs1 = state[ix1];
-            let gs2 = state[ix2];
-            let gs3 = state[ix3];
-            if(gs1 === gs2 && gs2 === gs3 && gs1 != ''){
-                win = true;
-            }  
+        winningConditions.forEach(c =>{
+            if(state[c[0]] === player && state[c[1]] === player 
+                && state[c[2]] === player ){
+                   win = true;
+            }
         })
         return win;
     }
-    const checkDraw = function(){
-        let draw = false;
-        let open = false;
-        gamestate.forEach(box =>{
-            if(box === ''){
-                open = true;
-            } 
-            // console.log({open});
-        })
-        if(!open && !checkWin()){
-            draw = true;
-        } 
-        return draw;
-    }
-    const clearGameState = function(){
-        gamestate = ['','','','','','','','',''];
-        renderGameState();
-    };
-    init();
-    return {
-        updateGameState,
-        clearGameState,
-        validateMove,
-        checkWin,
-        checkDraw,
-        getGameState
-    }
-})();
 
-const playerFactory = (name,marker)=>{
-    const getMarker = () => marker;
-    const getName = () => name;
-    const getComp = () => false;
-    return {
-        getMarker,
-        getName,
-        getComp
+    const checkDraw = function(state = gameState){
+        let movesLeft = getPossibleMoves().length > 1
+        let xWIN = checkWin('X')
+        let oWin = checkWin('O')
+        
+        if(!xWIN && !oWin && !movesLeft){
+            return true;
+        }
+        return false;
     }
-}
-const computerFactory = (name,marker)=>{
-    const getMarker = () => marker;
+    
+    const resetGameState = function(){
+        gameState = [0,1,2,3,4,5,6,7,8]
+    }
+
+    return{
+        getCurrentGameState,
+        getPossibleMoves,
+        updateGameState,
+        resetGameState,
+        checkDraw,
+        checkWin
+    };
+
+})();
+const PlayerFactory = (name, marker)=>{
     const getName = () => name;
-    const getComp = () => true;
+    const getMarker = () => marker;
+    const isComputer = () => false;
+    
+    return{
+        getName,
+        getMarker,
+        isComputer
+    }
+};
+
+const ComputerFactory = (name, marker,level)=>{
+    const prototype = PlayerFactory(name, marker);
+    const ai = marker
+    const lvl = level;
+    const human = ai === 'X' ? 'O':'X';
+    const isComputer = () => true;
     function getRandomArbitrary(min=0, max=9) {
         return Math.floor(Math.random() * (max - min) + min);
     }
-    const getValidMoves = function(){
-        let valid = false;
-        let validMoves = [];
-        for(let i = 0; i<9; i++){
-            valid = GameBoard.validateMove(i);
-            if(valid){
-                validMoves.push(i);
+    const makeDumbMove = function(){
+        let availSpots = GameBoard.getPossibleMoves();
+        let move = {}
+        let ix = getRandomArbitrary(0,availSpots.length);
+        move.index = availSpots[ix];
+        return move;
+    }
+
+    const makeSmartMove = function (){
+        if(lvl === 0){
+            return makeDumbMove()
+        }else if(lvl === 1){
+            let roll = getRandomArbitrary(1,100) 
+            if(roll>60){
+                return minimax(GameBoard.getCurrentGameState(), ai);
+            }else{
+                return makeDumbMove();
+            }
+        }else if(lvl === 2){
+            let roll = getRandomArbitrary(1,100) 
+            if(roll>20){
+                return minimax(GameBoard.getCurrentGameState(), ai);
+            }else{
+                return makeDumbMove();
             }
         }
-        return validMoves;
+        return minimax(GameBoard.getCurrentGameState(), ai);
     }
-    const makeMove = function(){
-        posMoves = getValidMoves();
-        let move;
-        let winningMove = false;
-        posMoves.forEach(choice =>{
-            let state = GameBoard.getGameState();
-            state[choice]=marker;
-                if(GameBoard.checkWin(state)){
-                    move =  choice;
-                    winningMove = true;
-                }
-            })
-            if(winningMove){
-                return move;
-            }
-            // ELSE CHOOSE RANDOM MOVE
-            console.log('nowins')
-            let moveChoice = getRandomArbitrary(0,posMoves.length);
-            move = posMoves[moveChoice];
-            return move;
-        
-    }
-    return {
-        getMarker,
-        getName,
-        makeMove,
-        getComp
-    }
-}
 
-const gameMaster = (()=>{
-    let player1
-    let player2
-    let currentPlayer;
-    const resetBtn = document.querySelector('#reset')
-    const init = function(p1, p2){
-        player1 = p1;
-        player2 = p2;
-        currentPlayer = player1;
-        bindEvents();
-        displayControl.updateDisplay(currentPlayer.getName());
+    const minimax = function (newBoard, player){
+        let availSpots = GameBoard.getPossibleMoves(newBoard);
+        if (GameBoard.checkWin(human, newBoard)){
+            return {score:-10};
+        }else if (GameBoard.checkWin(ai,newBoard)){
+            return {score:10};
+        }else if (availSpots.length === 0){
+             return {score:0};
+        };
+
+        let moves = [];
+        for (let i = 0; i < availSpots.length; i++){
+            let move = {};
+  	        move.index = newBoard[availSpots[i]];
+            newBoard[availSpots[i]] = player;
+            if(player == ai){
+                let result = minimax(newBoard, human);
+                move.score = result.score;
+            }else{
+                let result = minimax(newBoard, ai);
+                move.score = result.score;
+            }
+            newBoard[availSpots[i]] = move.index;
+            
+            moves.push(move);
+
+        }
+        let bestMove;
+        if(player === ai){
+            let bestSoFar = -10000;
+            for(let i = 0; i < moves.length; i++){
+                if(moves[i].score > bestSoFar){
+                    bestSoFar = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }else{
+            let bestSoFar = 10000;
+            for(let i = 0; i < moves.length; i++){
+                if(moves[i].score < bestSoFar){
+                    bestSoFar = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
+        return moves[bestMove];
     }
     
-    const reset = function(){
-        GameBoard.clearGameState();
-        currentPlayer = player1;
-        displayControl.updateDisplay(currentPlayer.getName());
+    return Object.assign({}, prototype, 
+        {
+        makeDumbMove,
+        makeSmartMove,
+        isComputer
+    })
+};
+
+const gameMaster = (()=>{
+    let player1;
+    let player2;
+    let currentPlayer;
+    let gameOn;
+    const resetBtn = document.querySelector('#reset')
+    const computerMove = async function (){
+        await new Promise(r => setTimeout(r, 600));
+        let compMove = currentPlayer.makeSmartMove();
+        playRound(compMove.index);
+        viewController.renderBoard();
+
     }
+    const init =  function(p1,p2){
+        player1 = p1
+        player2 = p2
+        currentPlayer = player1;
+        viewController.controlGameDisplay(`${currentPlayer.getName()}'s turn.`)
+        viewController.controlScoreBoard(player1,player2)
+        gameOn = true;
+        bindEvents();
+        if(currentPlayer.isComputer()){
+            computerMove();
+        }
+        
+    }
+
     const bindEvents = function(){
         boxes.forEach(box => {
-            // console.log(box);
             box.addEventListener('click', handleClicks);
         });
         resetBtn.addEventListener('click', reset);
-    };
+    }
+
+    const reset =  function(){
+        GameBoard.resetGameState();
+        viewController.renderBoard();
+        gameOn = true;
+        currentPlayer = player1
+        viewController.controlGameDisplay(`${currentPlayer.getName()}'s turn.`)
+        if(currentPlayer.isComputer()){
+            computerMove();
+        }
+
+    }
+
     const handleClicks = function(e){
-        // console.log(e);
-        let ix = e.target.dataset['ix'];
+        let ix = Number(e.target.dataset['ix']);
         playRound(ix);
     }
+
     const playRound = async function(ix){
-        if(GameBoard.validateMove(ix)){
-            let marker = currentPlayer.getMarker();
-            GameBoard.updateGameState(ix, marker);
-        
-            if(GameBoard.checkWin()){
-                displayControl.updateDisplay(`${currentPlayer.getName()} wins`,true) 
+        let pos = GameBoard.getPossibleMoves();
+        if(pos.includes(ix)&& gameOn){
+            GameBoard.updateGameState(ix,currentPlayer.getMarker());
+            viewController.renderBoard();
+            if(GameBoard.checkWin('X')||GameBoard.checkWin('O')){
+                viewController.controlGameDisplay(`${currentPlayer.getName()} Wins.`);
+                viewController.updateScores(currentPlayer);
+                viewController.controlScoreBoard(player1,player2);
+                gameOn = false;
                 return;
             }else if(GameBoard.checkDraw()){
-                displayControl.updateDisplay('Draw',true)
+                viewController.controlGameDisplay('Draw.');
+                viewController.updateScores(currentPlayer,true);
+                viewController.controlScoreBoard(player1,player2);
+                gameOn = false;
                 return;
             }
             currentPlayer = currentPlayer === player1 ? player2:player1;
-            displayControl.updateDisplay(currentPlayer.getName());
-            if(currentPlayer.getComp()){
-                await new Promise(r => setTimeout(r, 500));
-                playRound(currentPlayer.makeMove())
+            viewController.controlGameDisplay(`${currentPlayer.getName()}'s turn.`);
+            if(currentPlayer.isComputer()){
+                computerMove();
             }
-        } 
-    }  
-    
-    // init();
+        }
+    }
     return{
-        init,
-        reset
+        init
     }
 })();
 
-const formControl = (()=>{
+const formMaster = (()=>{
     const startBtn = document.querySelector('#start');
     const inputs = document.querySelectorAll('.inputs')
-    const handleClicks = function(){
-        let p1 = inputs[0].value;
-        let p1M = inputs[1].value;
-        let p2 = inputs[2].value;
-        let p2M = inputs[3].value;
-        let compSel = inputs[4].value;
-        let isComp = false;
-
-        if(compSel === 'comp'){
-            isComp = true;
-        }
-        let player1 = playerFactory(p1, p1M.toUpperCase());
-        let player2;
-
-        if(isComp){
-            player2 = computerFactory(p2, p2M.toUpperCase());
-        }else{
-            player2 = playerFactory(p2, p2M.toUpperCase());
-        }
-        if(validateForm(p1,p1M,p2,p2M,compSel)){
-            displayControl.formHandler();
-            gameMaster.init(player1,player2);
-        }
-    }
-    const validateForm = function(p1,p1M,p2,p2M,compSel){
-        let validated = true;
-        if(p1 === ""){
-            validated = false;
-        }
-        if(p1 === p2){
-            validated = false;
-        }
-        if(compSel === ''){
-            validated = false;
-        }
-        if(p1M === p2M){
-            validated = false;
-        }
-        if(p1M === "" || p1M.length > 1){
-            validated = false;
-        }
-        if(p2 === ""){
-            validated = false;
-        }
-        if(p2M === "" || p1M.length > 1){
-            validated = false;
-        }
-        return validated;
-    }
-    startBtn.addEventListener('click',handleClicks);
     
+    const validateForm = () => true;
+    const handleSubmit = function(e){
+        let p1 = inputs[0].value;
+        let p2 = inputs[2].value; 
+        let p1Mode = inputs[1].value 
+        let p2Mode = inputs[3].value 
+
+        let player1;
+        let player2;
+        if(p1Mode === 'human'){
+            player1 = PlayerFactory(p1, 'X'); 
+        }else{
+            player1 = ComputerFactory(p1, 'X', Number(p1Mode));
+        }
+        if(p2Mode === 'human'){
+            player2 = PlayerFactory(p2, 'O'); 
+        }else{
+            player2 = ComputerFactory(p2, 'O', Number(p2Mode));
+        }
+        
+        if(validateForm()){
+            viewController.revealBoard();
+            gameMaster.init(player1, player2);
+        }
+    }
+    startBtn.addEventListener('click',handleSubmit);
+    return{
+        validateForm
+    }
 })();
 
